d901d48bff08cda302c10bf5d1866834165af1e7
mutsuya628 <yredmrmec@gmail.com>
2025-07-28 01:24:51 +0900

    next.config修正

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone',
+//   env: {
+//     API_ENDPOINT: process.env.API_ENDPOINT,
+//   }
+// }
+
 const nextConfig = {
-  output: 'standalone',
-  env: {
-    API_ENDPOINT: process.env.API_ENDPOINT,
-  }
-}
+    output: 'standalone'
+      }
+
 
 module.exports = nextConfig
